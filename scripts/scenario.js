a4f3a4ab40281c4c910dd68fed96fa60f3c1e639
Nowlook4D <contact@nowlook.fr>
2024-09-11 12:41:34 +0200

    mvt des harnais

--- scripts/scenario.js
+++ scripts/scenario.js
@@ -1,11 +1,10 @@
 // const version = "1.12.1";
 const iframe = document.getElementById("api-frame");
-// var client = new window.Sketchfab(version, iframe);
 var client = new Sketchfab( iframe );
-// var client = new Sketchfab("1.12.1", iframe);
 let api;
-const uid = "3c97912b6c0d4193a963f253ade24749";
-// client.init(uid, { autostart: 1, autospin: 0, success: success, error: error });
+// const uid = "3c97912b6c0d4193a963f253ade24749"; //busTour7
+const uid = "cb264ca05beb4abfb696514612734112";
+
 
 var sontEgaux = false;
 var assyBoitier = 0;
@@ -33,7 +32,6 @@ const ObjetsScenario = [
   [3, 623 ], //face avant
   [3, 397]   //boitier
 ];
-const ObjetsHide = [ [493],[477], [1061],[1069],[1053], [1027],[1035], [697],[689],           [473],[465],   [1003],[995 ],[987 ],    [675],[667],     [961],[969],     [251],[243],     [172],[183]  ];  //les harnais à cacher sur la table d'assemblage
 
 //---------------------------------------------
 function openPopup() { // Fonction pour ouvrir le popup
@@ -76,10 +74,12 @@ const success = (apiClient) => {
     api.addEventListener("viewerready", () => {
         api.getNodeMap(function(err, nodes) {
           if (!err) {window.console.log(nodes); }
-        });         
-        for (let h = 0; h < ObjetsHide.length; h++) {
-          api.hide (ObjetsHide[h]-2, function(err) {});
-        };
+        });
+
+        // const ObjetsHide = [ [493],[477], [1061],[1069],[1053], [1027],[1035], [697],[689],           [473],[465],   [1003],[995 ],[987 ],    [675],     [961],[969],     [251],[243],     [172],[183]  ];  //les harnais à cacher sur la table d'assemblage
+        // for (let h = 0; h < ObjetsHide.length; h++) {
+        //   api.hide (ObjetsHide[h]-2, function(err) {});
+        // };
 
       //-----------------------------------------------------------------------------------------------------------------------
       const Anot = [   // pour createAnnotationFromWorldPosition( annotation position, camera position (eye) and target, titleFR, textFR, titleEN, textEN, [callback] )
@@ -110,28 +110,74 @@ const success = (apiClient) => {
       api.setAnnotationsTexture(url, function () {});
       //-----------------------------------------------------------------------------------------------------------------------
       const obj = [ //ID selectionnable, ID ref, XYZ ref, famille
-        ['A3-01', -.4806, 0, -.7379, 845 , 853 ,869 ,861 ], //boitier 1U v2
-        ['A1-01',-.3017, 0, -.7228, 115, 123, 135, 146 ],   // boitier 2U
-        ['A2-01', -.1130, 0, -.7295, 569 ,585 ,577 ,0  ],   //boitier 1U v1
+        ['A3-01', -.4806, 0, -.7379, 845 , 853 ,869 ,861,0 ], //boitier 1U v2
+        ['A1-01',-.3017, 0, -.7228, 115, 123, 135, 146,0 ],   // boitier 2U
+        ['A2-01', -.1130, 0, -.7295, 569 ,585 ,577 ,0,0  ],   //boitier 1U v1
         // table devant à gauche
-        ['A1-02',-.8675, 0, -.308, 5 ,0 ,0 ,0  ],     // face avant 2U
-        ['A2-02',-.6924, 0, -.3063, 607 ,0 ,0 ,0  ], // face avant  1U v1
-        ['A3-02', -.539, 0, -.3225, 939 ,879 ,0 ,0  ],  // face avant  1U v2
+        ['A1-02',-.8675, 0, -.308, 5 ,0 ,0 ,0,0  ],     // face avant 2U
+        ['A2-02',-.6924, 0, -.3063, 607 ,0 ,0 ,0,0  ], // face avant  1U v1
+        ['A3-02', -.539, 0, -.3225, 939 ,879 ,0 ,0,0  ],  // face avant  1U v2
 
-        ['A3-12',-.432, 0, -.2653, 917 ,0 ,0 ,0  ],     // bouton de gauche
-        ['A1-14',-.4431, 0, -.27485, 37 ,0 ,0 ,0  ], // bouton de droite
+        ['A3-12',-.432, 0, -.2653, 917 ,0 ,0 ,0,0  ],     // bouton de gauche
+        ['A1-14',-.4431, 0, -.27485, 37 ,0 ,0 ,0,0  ], // bouton de droite
 
-        ['A2-13', -.411, 0, -.2974, 645 ,651 ,0 ,0  ],  // interrupteur
-        ['A3-11', -.40166, 0, -.2764, 895 ,901 ,0 ,0  ],  // interrupteur
+        ['A2-13', -.411, 0, -.2974, 645 ,651 ,0 ,0,0  ],  // interrupteur
+        ['A3-11', -.40166, 0, -.2764, 895 ,901 ,0 ,0,0  ],  // interrupteur
 
         ['A1-13', -.4497, 0, -.3033, 71 ,0 ,0 ,0  ],  // potar
-        ['A1-12', -.45595, 0, -.3212, 49 ,0 ,0 ,0  ],  // potar
-        ['A1-11', -.4494, 0, -.4228, 93 ,0 ,0 ,0  ],  // potar
+        ['A1-12', -.45595, 0, -.3212, 49 ,0 ,0 ,0,0  ],  // potar
+        ['A1-11', -.4494, 0, -.4228, 93 ,0 ,0 ,0,0  ],  // potar
 
         // table devant à droite
-        ['A3-32',.4675, 0, -.3822, 715  ,0 ,0 ,0  ],     // backshell cylindrique
-        ['A2-15',.4903, 0, -.33515, 1275 ,0 ,0 ,0  ]     // backshell  rectangulaire
+        ['A3-32',.4675, 0, -.3822, 715  ,0 ,0 ,0,0  ],     // backshell cylindrique
+        ['A2-15',.4903, 0, -.33515, 1275 ,0 ,0 ,0,0  ],     // backshell  rectangulaire
+        ['A2-14',.527, 0, -.3733, 741  ,747 ,0 ,0,0  ],     // connecteur cylindrique
+        ['A1-43',.6328, -.05, -.3761, 527 ,539 ,0 ,0,0  ],     // connecteur optimus
+        ['A3-34',.6176, 0, -.3164, 1289 ,1313 ,0 ,0,0  ],     // connecteur rectangulaire
+
+        ['A1-33',.6328, .044, -.2958,     423,439,    477,493,0 ],     // harnais optimus à plat
+        ['A1-23',.6328, -.3, -.3761,     477,493,    423,439,0],     // harnais optimus en forme
+        ['A1-18',.7537, -.023, -.3752,   363,369,      0,0,0 ],     // connecteur A1-18
+
+        ['A3-31',.6296, .044, -.2958,   1151,1167,1159,    1061,1069],    // harnais rectangulaire à plat
+        ['A3-21',.6176, -.3, -.3164,  1053,1069,1061,       1159,1167],     // harnais rectangulaire en forme 1053
+        ['A3-13',.7216, .0, -.3346,    1005,1011,    0,0,0 ],       // connecteur A3-13
+
+        ['hhh',.7749, 0, -.3672,   1547,1553,   0,0,0],    // harnais fake
+        ['hhh',.7749, 0, -.3672,   1569,1575,    0,0,0],    // harnais fake
+        ['hhh',.7749, 0, -.3672,    1611,1617,    0,0,0],    // harnais fake
+
+        // table arrière à droite
+        ['AMM',.3399, 0, -.7084,   1341,1353,   0,0,0],    // connecteur fake
+        ['CMM',.43, 0, -.8567,   1525,0,    0,0,0],    // connecteur fake
+        ['CMM',.43, 0, -.8567,   1481,0,    0,0,0],    // connecteur fake
+        ['EMM',.4958, 0, -.7012,   1373,1379,1385,1391,0],    // connecteur fake
+
+        ['A3-33',.3637, .03, -.5579,     1249,1257,    1027,1035,0 ],     // harnais
+        ['A3-22',.2783, -.3, -.6918,     1027,1035,    1249,1257,0],     // harnais en forme
+        ['A3-15',.2953, -.015, -.6951,   961,0,      0,0,0 ],     // connecteur 
+        ['A3-14',.2707, -.015, -.6891,   983,0,      0,0,0 ],     // connecteur
+
+        ['A1-31',.3637, .03, -.5579,     385,393,    172,183,0 ],     // harnais
+        ['A1-21',.3037, -.3, -.5924,     172,183,    385,393,0],     // harnais en forme
+        ['A1-17',.2909, -.048, -.5921,   217,225,      0,0,0 ],     // connecteur
+
+        ['A2-31',.3637, .031, -.5579,      763,771,779,    689,697],     // harnais
+        ['A2-21',.4472, -.3, -.6456,       689,697,    763,771,779],     // harnais en forme
+        ['A2-12.1',.3768, -.019, -.6372,   801,807,      0,0,0 ],     // connecteur
+        ['A2-12',.3767, -.019, -.7182,     667,673,      0,0,0 ],     // connecteur
+        ['A2-12.2',.5702, -.019, -.6403,   823,829,     0,0,0 ],     // connecteur
+
+        ['A1-32',.3637, .031, -.5579,    325,333,341,    689,697],     // harnais
+        ['A1-22',.5019, -.3, -.6091,     243,251,    325,333,341],     // harnais en forme 259
+        ['A1-15',.4891, -.043, -.6186,   281,287,      0,0,0 ],     // connecteur
+        ['A1-16',.5056, -.043, -.595,    303,309,      0,0,0 ],     // connecteur
+
       ];
+
+
+
+
       //---------------------------------------------
       api.addEventListener('click',function(info) { 
         window.console.log('clicked node', info.instanceID);
@@ -143,7 +189,7 @@ const success = (apiClient) => {
             if (info.position3D[1] < 0 ) { // l'objet est sur la table (position Y < 0) alors retour à sa position initiale
               for (let i = 0; i < obj.length; i++) {
                 var clic = info.instanceID;
-                if ((clic == obj[i][4]) || (clic == obj[i][5]) || (clic == obj[i][6]) || (clic == obj[i][7])) { // translate( instanceID, translateTo XYZ, options, [callback] )
+                if ((clic == obj[i][4]) || (clic == obj[i][5]) || (clic == obj[i][6]) || (clic == obj[i][7]) || (clic == obj[i][8])) { // translate( instanceID, translateTo XYZ, options, [callback] )
                   api.translate(obj[i][4]-2, [obj[i][1], obj[i][2], obj[i][3]], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
                   tableAssy = tableAssy.filter((element) => element !== obj[i][4]);
                   window.console.log("objets sur la table :", tableAssy)
@@ -152,12 +198,12 @@ const success = (apiClient) => {
                 };
               };
 
-
             } else { // l'objet n'est pas sur la table
               for (let i = 0; i < obj.length; i++) {
                 var clic = info.instanceID;
-                if ((clic == obj[i][4]) || (clic == obj[i][5]) || (clic == obj[i][6]) || (clic == obj[i][7])) {
+                if ((clic == obj[i][4]) || (clic == obj[i][5]) || (clic == obj[i][6]) || (clic == obj[i][7]) || (clic == obj[i][8])) {
                   api.translate(obj[i][4]-2, [0, 0, 0], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
+                  // if ((clic == obj[i][4]) || (clic == obj[i][5])) {api.translate(obj[i][8]-2, [0, 0, 0], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});} //cache le harnais en forme
                   total = tableAssy.push(obj[i][4]);
                   window.console.log("objets sur la table :", tableAssy);
                   if ((i>-1) && (i<3)) { // c'est un boîtier
@@ -173,27 +219,26 @@ const success = (apiClient) => {
                     for (let j = 0; j < 4; j++) { lastFaceAvant[j] = obj[i][j+1] }; 
                   };
                 };
-
-
               };
-              api.setCameraLookAt([0, -1, .7], [0, -.2, .3], 4.3, function(err) {}); //setCameraLookAt( position, target, [duration], [callback] )
-              // if (info.instanceID == 212 ) {
-              //   api.hide (212, function(err) {}); //212
-              //   api.hide (204, function(err) {}); 
-              //   api.show (186, function(err) {}); //186
+              // api.setCameraLookAt([0, -1, .7], [0, -.2, .3], 4.3, function(err) {}); //setCameraLookAt( position, target, [duration], [callback] )
+            };
+
+
+              // if (info.instanceID == 779 ) { // clic sur le harnais A2 à plat
+              //   api.hide (763-2, function(err) {});
+              //   api.show (689-2, function(err) {});
+              //   api.translate(667-2, [0, 0, 0], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
+              //   api.translate(801-2, [0, 0, 0], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
+              //   api.translate(823-2, [0, 0, 0], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
+              // };
+              // if (info.instanceID == 697 ) {  // clic sur le harnais A2 en forme sur la table d'assemblage
+              //   api.show (763-2, function(err) {});
+              //   api.hide (689-2, function(err) {});
+              //   api.translate(667-2, [.5733, -.02, -.6407], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
+              //   api.translate(801-2, [.3761, -.02, -.6372], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
+              //   api.translate(823-2, [.3299, -.02, -.6306], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
               // };
 
-            };
-            //----Famille des boitiers electroniques-----------------------------------------
-            // for (let i = 0; i < obj.length; i++) {
-            //   if (obj[i][0] == info.instanceID) {
-            //     api.translate(obj[i][1], [0, 0, 0], {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
-            //     api.translate(IDavant, XYZavant, {duration: .2, easing: 'easeOutQuad'}, function(err, translateTo) {});
-            //     IDavant = obj[i][1]; //sauvegarde de la position table composant
-            //     XYZavant = [obj[i][2], obj[i][3], obj[i][4]];
-            //   };
-            // };
-            //---------------------------------------------
           };
           //---------------------------------------------
           if (info.instanceID == 913) { //clic sur le buzzer => vérification de la correspondance entre les objets sur table et la consigne
